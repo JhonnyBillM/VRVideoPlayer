b0554903e1adf6aa87b7f4b4426c5f927052e718
Jhonny Bill <jhonnybillm@gmail.com>
2019-07-04 08:37:22 -0400

    ADD - FullScreenButton+Extension file.

---
+++ VRVideoPlayer/FullScreenButton+Extension.swift
@@ -0,0 +1,9 @@
+//
+//  FullScreenButton+Extension.swift
+//  VRVideoPlayer
+//
+//  Created by Jhonny Bill Mena on 7/4/19.
+//  Copyright © 2019 com.monteroc. All rights reserved.
+//
+
+import Foundation
